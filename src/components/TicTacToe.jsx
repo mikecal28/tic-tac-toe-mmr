674e08eb3e505b45bc0db3700520ce2332265e52
mikecal28 <mt28.caldwell@gmail.com>
2024-04-16 16:13:25 -0600

    extracts minimax logic from tic tac toe game into its own hook

--- src/components/TicTacToe.jsx
+++ src/components/TicTacToe.jsx
@@ -1,9 +1,8 @@
-import { useCallback, useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import useMiniMax from "../hooks/useMiniMax";
 
 function TicTacToe() {
-  const [currentPlayer, setCurrentPlayer] = useState("X");
-  const [endMessage, setEndMessage] = useState("");
   const [gameBoard, setGameBoard] = useState([
     "",
     "",
@@ -16,95 +15,7 @@ function TicTacToe() {
     "",
   ]);
 
-  const virtualBoard = useRef(["", "", "", "", "", "", "", "", ""]);
-
-  const makeVirtualMove = useCallback((position, player) => {
-    if (isValidMove(position)) {
-      const boardCopy = [...virtualBoard.current];
-      boardCopy.splice(position, 1, player);
-      virtualBoard.current = boardCopy;
-
-      return true;
-    }
-    return false;
-  }, []);
-
-  const minimax = useCallback(
-    (isMaximizing) => {
-      let winner = checkWin();
-      if (winner) {
-        return { score: { X: -1, O: 1, tie: 0 }[winner] };
-      }
-
-      if (isMaximizing) {
-        let bestScore = -2;
-        const validMoves = getValidMoves();
-        validMoves.forEach((move, idx) => {
-          makeVirtualMove(move, "O");
-          let score = minimax(false)["score"];
-          virtualBoard.current[move] = "";
-          if (score > bestScore) {
-            bestScore = score;
-          }
-        });
-        return { score: bestScore };
-      } else {
-        let bestScore = 2;
-        const validMoves = getValidMoves();
-        validMoves.forEach((move, idx) => {
-          makeVirtualMove(move, "X");
-          let score = minimax(true)["score"];
-
-          virtualBoard.current[move] = "";
-
-          if (score < bestScore) {
-            bestScore = score;
-          }
-        });
-        return { score: bestScore };
-      }
-    },
-    [makeVirtualMove]
-  );
-
-  const findBestMove = useCallback(() => {
-    let bestScore = -2;
-    let moves = [];
-    let bestMoves = [];
-
-    getValidMoves().forEach((move, idx) => {
-      makeVirtualMove(move, "O");
-      let score = minimax(false)["score"];
-      virtualBoard.current[move] = "";
-      moves.push([score, move]);
-      if (score > bestScore) {
-        bestScore = score;
-      }
-    });
-
-    moves.forEach((move) => {
-      if (move[0] === bestScore) {
-        bestMoves.push(move[1]);
-      }
-    });
-
-    return bestMoves[Math.floor(Math.random() * bestMoves.length)];
-  }, [makeVirtualMove, minimax]);
-
-  const makeMove = useCallback(
-    (position, player) => {
-      if (isValidMove(position)) {
-        const boardCopy = [...gameBoard];
-        boardCopy.splice(position, 1, player);
-        setGameBoard(boardCopy);
-        virtualBoard.current = boardCopy;
-
-        return true;
-      }
-      return false;
-    },
-    [gameBoard]
-  );
+  const { reset, makeMove, endMessage } = useMiniMax(gameBoard, setGameBoard);
 
   const renderGameSquares = () =>
     gameBoard.map((square, idx) => (
@@ -117,96 +28,6 @@ function TicTacToe() {
         {square}
       </div>
     ));
-
-  function isValidMove(position) {
-    return !virtualBoard.current[position];
-  }
-
-  function checkWin() {
-    const winningCombinations = [
-      [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8],
-      [0, 3, 6],
-      [1, 4, 7],
-      [2, 5, 8],
-      [0, 4, 8],
-      [2, 4, 6],
-    ];
-
-    let winner = false;
-    winningCombinations.forEach((combo) => {
-      let a = combo[0];
-      let b = combo[1];
-      let c = combo[2];
-
-      if (
-        virtualBoard.current[a] &&
-        virtualBoard.current[a] === virtualBoard.current[b] &&
-        virtualBoard.current[a] === virtualBoard.current[c]
-      ) {
-        winner = virtualBoard.current[a];
-      }
-    });
-
-    if (!winner && !virtualBoard.current.includes("")) {
-      return "tie";
-    }
-
-    return winner;
-  }
-
-  function getValidMoves() {
-    return [0, 1, 2, 3, 4, 5, 6, 7, 8].filter(
-      (move) => !virtualBoard.current[move]
-    );
-  }
-
-  function reset() {
-    setGameBoard(["", "", "", "", "", "", "", "", ""]);
-    virtualBoard.current = ["", "", "", "", "", "", "", "", ""];
-    setEndMessage("");
-  }
-
-  useEffect(() => {
-    if (currentPlayer === "O" && !endMessage) {
-      const timeOut = () =>
-        setTimeout(() => {
-          let move = findBestMove();
-          if (move || move === 0) {
-            makeMove(move, "O");
-          }
-
-          let win = checkWin();
-          console.log("win: ", win);
-
-          if (win || getValidMoves().length === 0) {
-            if (win) {
-              setEndMessage(`${win} wins!`);
-            } else {
-              setEndMessage("It's a tie!");
-            }
-          } else {
-            setCurrentPlayer("X");
-          }
-        }, 2000);
-
-      timeOut();
-
-      clearTimeout(timeOut);
-    }
-  }, [currentPlayer, findBestMove, makeMove, endMessage]);
-
-  useEffect(() => {
-    const X = gameBoard.filter((char) => char === "X");
-    const O = gameBoard.filter((char) => char === "O");
-
-    if (X.length > O.length) {
-      setCurrentPlayer("O");
-    } else {
-      setCurrentPlayer("X");
-    }
-  }, [gameBoard]);
 
   return (
     <div className="tic-tac-toe-container">
--- src/hooks/useMiniMax.js
+++ src/hooks/useMiniMax.js
@@ -1,6 +1,190 @@
-export default function useMiniMax() {
-  
-  return (  );
-}
+import { useCallback, useEffect, useRef, useState } from "react";
 
- 
+export default function useMiniMax(gameBoard, setGameBoard) {
+  const [currentPlayer, setCurrentPlayer] = useState("X");
+  const [endMessage, setEndMessage] = useState("");
+
+  const baseGameBoard = useRef([...gameBoard]);
+  const virtualBoard = useRef([...gameBoard]);
+
+  const makeVirtualMove = useCallback((position, player) => {
+    if (isValidMove(position)) {
+      const boardCopy = [...virtualBoard.current];
+      boardCopy.splice(position, 1, player);
+      virtualBoard.current = boardCopy;
+
+      return true;
+    }
+    return false;
+  }, []);
+
+  const minimax = useCallback(
+    (isMaximizing) => {
+      let winner = checkWin();
+      if (winner) {
+        return { score: { X: -1, O: 1, tie: 0 }[winner] };
+      }
+
+      if (isMaximizing) {
+        let bestScore = -2;
+        const validMoves = getValidMoves();
+        validMoves.forEach((move) => {
+          makeVirtualMove(move, "O");
+          let score = minimax(false)["score"];
+          virtualBoard.current[move] = "";
+          if (score > bestScore) {
+            bestScore = score;
+          }
+        });
+        return { score: bestScore };
+      } else {
+        let bestScore = 2;
+        const validMoves = getValidMoves();
+        validMoves.forEach((move) => {
+          makeVirtualMove(move, "X");
+          let score = minimax(true)["score"];
+
+          virtualBoard.current[move] = "";
+
+          if (score < bestScore) {
+            bestScore = score;
+          }
+        });
+        return { score: bestScore };
+      }
+    },
+    [makeVirtualMove]
+  );
+
+  const findBestMove = useCallback(() => {
+    let bestScore = -2;
+    let moves = [];
+    let bestMoves = [];
+
+    getValidMoves().forEach((move, idx) => {
+      makeVirtualMove(move, "O");
+      let score = minimax(false)["score"];
+      virtualBoard.current[move] = "";
+      moves.push([score, move]);
+      if (score > bestScore) {
+        bestScore = score;
+      }
+    });
+
+    moves.forEach((move) => {
+      if (move[0] === bestScore) {
+        bestMoves.push(move[1]);
+      }
+    });
+
+    return bestMoves[Math.floor(Math.random() * bestMoves.length)];
+  }, [makeVirtualMove, minimax]);
+
+  const makeMove = useCallback(
+    (position, player) => {
+      if (isValidMove(position)) {
+        const boardCopy = [...gameBoard];
+        boardCopy.splice(position, 1, player);
+        setGameBoard(boardCopy);
+        virtualBoard.current = boardCopy;
+
+        return true;
+      }
+      return false;
+    },
+    [gameBoard, setGameBoard]
+  );
+
+  function isValidMove(position) {
+    return !virtualBoard.current[position];
+  }
+
+  function checkWin() {
+    const winningCombinations = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+
+    let winner = false;
+    winningCombinations.forEach((combo) => {
+      let a = combo[0];
+      let b = combo[1];
+      let c = combo[2];
+
+      if (
+        virtualBoard.current[a] &&
+        virtualBoard.current[a] === virtualBoard.current[b] &&
+        virtualBoard.current[a] === virtualBoard.current[c]
+      ) {
+        winner = virtualBoard.current[a];
+      }
+    });
+
+    if (!winner && !virtualBoard.current.includes("")) {
+      return "tie";
+    }
+
+    return winner;
+  }
+
+  function getValidMoves() {
+    return [0, 1, 2, 3, 4, 5, 6, 7, 8].filter(
+      (move) => !virtualBoard.current[move]
+    );
+  }
+
+  function reset() {
+    setGameBoard([...baseGameBoard.current]);
+    virtualBoard.current = [...baseGameBoard.current];
+    setEndMessage("");
+    setCurrentPlayer("X");
+  }
+
+  useEffect(() => {
+    if (currentPlayer === "O" && !endMessage) {
+      const timeOut = () =>
+        setTimeout(() => {
+          let move = findBestMove();
+          if (move || move === 0) {
+            makeMove(move, "O");
+          }
+
+          let win = checkWin();
+          console.log("win: ", win);
+
+          if (win || getValidMoves().length === 0) {
+            if (win) {
+              setEndMessage(`${win} wins!`);
+            } else {
+              setEndMessage("It's a tie!");
+            }
+          } else {
+            setCurrentPlayer("X");
+          }
+        }, 2000);
+
+      timeOut();
+
+      clearTimeout(timeOut);
+    }
+  }, [currentPlayer, findBestMove, makeMove, endMessage]);
+
+  useEffect(() => {
+    const X = gameBoard.filter((char) => char === "X");
+    const O = gameBoard.filter((char) => char === "O");
+
+    if (X.length > O.length) {
+      setCurrentPlayer("O");
+    } else {
+      setCurrentPlayer("X");
+    }
+  }, [gameBoard]);
+
+  return { reset, makeMove, endMessage };
+}
